ffeb84da856f7da8f27ac2189f163d825ea3edee
Evan Krupa <123339863+EKrupa@users.noreply.github.com>
2024-03-03 11:29:05 -0500

    Add files via upload

---
+++ inventory/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Index, SignUpView, Dashboard, AddItem, logout_user, EditItem, DeleteItem
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', Index.as_view(), name='index'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+    path('add-item/', AddItem.as_view(), name='add-item'),
+    path('edit-item/<int:pk>', EditItem.as_view(), name='edit-item'),
+    path('delete-item/<int:pk>', DeleteItem.as_view(), name='delete-item' ),
+    path('signup/', SignUpView.as_view(), name="signup"),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html') , name ='login'), 
+    path('logout/', logout_user, name='logout'),
+    #path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+
+]
